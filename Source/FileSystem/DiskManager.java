d17bdcfcd005afcae48e5b065e81d2ecf8285126
Grzegorz Piątkowski <grzegorz.piatkowski@hotmail.com>
2020-01-12 22:36:33 +0100

    static

--- Source/FileSystem/DiskManager.java
+++ Source/FileSystem/DiskManager.java
@@ -17,4 +17,12 @@ public class DiskManager extends Disk {
         }
         return bitMap;
     }
+
+    public static void showDisk(char[][] disk){
+        for(int i = 0; i < bitMap.length; i++){
+            for(int j = 0; j < blockSize; j++){
+                System.out.println(disk[i][j]);
+            }
+        }
+    }
 }
