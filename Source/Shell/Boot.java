b37ea2e379efeb3718ba827ffced5ccaae890392
Grzegorz Piątkowski <grzegorz.piatkowski@hotmail.com>
2020-01-15 16:24:56 +0100

    dziala boot

--- Source/Shell/Boot.java
+++ Source/Shell/Boot.java
@@ -1,10 +1,12 @@
 package Shell;
 
+import Process.ProcessMenager;
 import Scheduler.Scheduler;
 public class Boot
 {
 
     public static void main(String[] args) {
+        ProcessMenager.init();
         Scheduler.set_init();
         Shell roboczy;
         roboczy = new Shell();
