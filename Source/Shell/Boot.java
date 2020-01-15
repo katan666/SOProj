c0bdcbc04a17f4ae93e86322439175bedadd4048
katan666 <dominik.bola1@wp.pl>
2020-01-15 20:40:19 +0100

    Drobne zmiany

--- Source/Shell/Boot.java
+++ Source/Shell/Boot.java
@@ -2,12 +2,15 @@ package Shell;
 
 import Process.ProcessMenager;
 import Scheduler.Scheduler;
+import RAM.MMU.MemoryManager;
+
 public class Boot
 {
 
     public static void main(String[] args) {
         ProcessMenager.init();
         Scheduler.set_init();
+        MemoryManager.init();
         Shell roboczy;
         roboczy = new Shell();
         roboczy.loopStart();
