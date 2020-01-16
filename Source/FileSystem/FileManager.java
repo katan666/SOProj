e867051d602fe85c51ada79acbea733a4f4b618f
Grzegorz Piątkowski <grzegorz.piatkowski@hotmail.com>
2020-01-16 02:17:14 +0100

    drobne techniczne

--- Source/FileSystem/FileManager.java
+++ Source/FileSystem/FileManager.java
@@ -6,108 +6,8 @@ import Process.*;
 
 public class FileManager extends Files{
 
-    public static void main(String[] args) {
-        String l = "l";
-        String r;
-        fillDisk();
-        createFile(l);
-        openFile(l);
-        writeFile(l, "Siema siema o tej porze kazdy wypic moze");
-        int k = fileLength(l);
-        showDisk();
-        showBitMap();
-        showMainCatalog();
-        showOpenedFiles();
-        r = readFile(l, 39);
-        System.out.println(k);
-        System.out.println(r);
-        closeFile(l);
-        showOpenedFiles();
-        deleteFile(l);
-        showMainCatalog();
-    }
-
     public static Vector<Files> mainCatalog = new Vector<Files>();
     public static Vector<Character> openedFiles = new Vector<Character>();
-
-
-    //stare adresy
-    /*
-    public static int readAddress(char index){
-        int address = -1;
-        if(index == '0')address = 0;
-        if(index == '1')address = 1;
-        if(index == '2')address = 2;
-        if(index == '3')address = 3;
-        if(index == '4')address = 4;
-        if(index == '5')address = 5;
-        if(index == '6')address = 6;
-        if(index == '7')address = 7;
-        if(index == '8')address = 8;
-        if(index == '9')address = 9;
-        if(index == 'a')address = 10;
-        if(index == 'b')address = 11;
-        if(index == 'c')address = 12;
-        if(index == 'd')address = 13;
-        if(index == 'e')address = 14;
-        if(index == 'f')address = 15;
-        if(index == 'g')address = 16;
-        if(index == 'h')address = 17;
-        if(index == 'i')address = 18;
-        if(index == 'j')address = 19;
-        if(index == 'k')address = 20;
-        if(index == 'l')address = 21;
-        if(index == 'm')address = 22;
-        if(index == 'n')address = 23;
-        if(index == 'o')address = 24;
-        if(index == 'p')address = 25;
-        if(index == 'q')address = 26;
-        if(index == 'r')address = 27;
-        if(index == 's')address = 28;
-        if(index == 't')address = 29;
-        if(index == 'u')address = 30;
-        if(index == 'v')address = 31;
-        if(index == 'w')address = 32;
-        return address;
-    }
-    public static char writeAddress(int address){
-        char index = '-';
-        if(address == 0)index = '0';
-        if(address == 1)index = '1';
-        if(address == 2)index = '2';
-        if(address == 3)index = '3';
-        if(address == 4)index = '4';
-        if(address == 5)index = '5';
-        if(address == 6)index = '6';
-        if(address == 7)index = '7';
-        if(address == 8)index = '8';
-        if(address == 9)index = '9';
-        if(address == 10)index = 'a';
-        if(address == 11)index = 'b';
-        if(address == 12)index = 'c';
-        if(address == 13)index = 'd';
-        if(address == 14)index = 'e';
-        if(address == 15)index = 'f';
-        if(address == 16)index = 'g';
-        if(address == 17)index = 'h';
-        if(address == 18)index = 'i';
-        if(address == 19)index = 'j';
-        if(address == 20)index = 'k';
-        if(address == 21)index = 'l';
-        if(address == 22)index = 'm';
-        if(address == 23)index = 'n';
-        if(address == 24)index = 'o';
-        if(address == 25)index = 'p';
-        if(address == 26)index = 'q';
-        if(address == 27)index = 'r';
-        if(address == 28)index = 's';
-        if(address == 29)index = 't';
-        if(address == 30)index = 'u';
-        if(address == 31)index = 'v';
-        if(address == 32)index = 'w';
-        return index;
-    }
-*/
 
 
     private final static char BEGIN = '0';
@@ -180,7 +80,8 @@ public class FileManager extends Files{
         return fileLength;
     }
 
-/*
+
+    /*
     public static int getWritePointer(String name){
         int writePointer = 0;
         for (int i = 0; i < mainCatalog.size(); i++) {
@@ -225,7 +126,8 @@ public class FileManager extends Files{
             }
         }
     }
-*/
+    */
+
 
     private static char getIndexBlock(String name){
         char index = '-';
--- Source/Process/OpenFile.java
+++ Source/Process/OpenFile.java
@@ -5,12 +5,12 @@ import FileSystem.Files;
 public class OpenFile {
 
     public Files file;
-    public int readPointer;
-    public int writePointer;
+    //public int readPointer;
+    //public int writePointer;
 
     public OpenFile(Files file2) {
         this.file = file2;
-        this.readPointer = 0;
-        this.writePointer = 0;
+        //this.readPointer = 0;
+        //this.writePointer = 0;
     }
 }
